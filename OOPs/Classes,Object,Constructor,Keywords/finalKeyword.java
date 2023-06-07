2d78eb90b67270dfa1f9395589802d8b8328c6e5
Vikash Gorai <114097162+vikashgorai@users.noreply.github.com>
2023-06-08 01:40:42 +0530

    Create finalKeyword.java

---
+++ OOPs/Classes,Object,Constructor,Keywords/finalKeyword.java
@@ -0,0 +1,28 @@
+public class finalkeyword {
+    public static void main(String[] args) {
+        Student Vikash = new Student(22,99.9f,"VV");
+        Student random = new Student(Vikash); 
+        // final int A =5;         
+        //  A =6; // error value of A will not change  
+        final Student Arpit = new Student(2, 99.9f, "Arpit");
+        Arpit.marks = 33; // final keyword does not work on non primitive datatype we can change the value of object but we cant change the object to other object 
+        //refrence variable of object will always point toward the same object.
+         Arpit = Vikash; // this will give an error because of final keyword,  refrence variable of arpit is pointing towards object vikash
+    }
+}
+class Student{
+    int marks;
+    float percent;
+    String name;
+
+    Student(Student other){
+        this.marks = other.marks;
+    }
+
+    Student(int marks,float percent,String name){
+         this.marks = marks;
+         this.percent = percent;
+         this.name = name;
+    }
+  
+}
