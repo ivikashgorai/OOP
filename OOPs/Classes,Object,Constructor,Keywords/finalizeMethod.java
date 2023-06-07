c8e12f9ab26f084bedf40b43b34c792ead7b0f6a
Vikash Gorai <114097162+vikashgorai@users.noreply.github.com>
2023-06-08 01:31:11 +0530

    Create finalizeMethod.java

---
+++ OOPs/Classes,Object,Constructor,Keywords/finalizeMethod.java
@@ -0,0 +1,25 @@
+public class CallConstructorfromanotherConstructor {
+    public static void main(String[] args) {
+        Student Vikash;
+        for (int i = 0; i < 1000000000; i++) {
+            Vikash = new Student(33,"Hit" ,99.8f); // so many objects are creating at one by one so java have to garbage it after use
+            System.out.println(Vikash.marks);
+        }
+      
+    }
+}
+class Student{
+    int marks;
+    String name;
+    float percent;
+ 
+    Student(int marks,String name,float percent){
+            this.marks = marks;
+            this.name = name;
+            this.percent = percent;
+    }
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Vikash"); // in last it will print vikash
+    }
+}
