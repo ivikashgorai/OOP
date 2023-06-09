ef3bebb95e25c482496f48282dae0e0b81e1b07c
Hammer <114097162+hammerkingitself@users.noreply.github.com>
2023-06-10 03:06:04 +0530

    Human.java

---
+++ OOPs/static/Human.java
@@ -0,0 +1,12 @@
+public class Human {
+    int age;
+    String name;
+    int height;
+    static long population;
+    Human(int age,String name,int height){
+        this.age = age;
+        this.height = height;
+        this.name = name;
+        Human.population +=1; // this. also works but it does not make sense
+    }
+}
