5036dd4a23e21928751b5fd65fd45a95f2528883
Vikash Gorai <114097162+vikashgorai@users.noreply.github.com>
2023-06-09 19:57:16 +0530

    Create a.java

---
+++ OOPs/packages/a.java
@@ -0,0 +1,10 @@
+package packages.a;
+
+public class Greeting {  // we cant make same class with name Greeting in same directory "a"
+    public static void main(String[] args) {
+        System.out.println("ccf");
+    }
+    public static void message(){
+        System.out.println("HII");
+    }
+}
